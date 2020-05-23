eb99ad9d3fdb7c3ff55fe8736214d3dda6960162
KantaiMishima <mishimakantai@gmail.com>
2020-05-23 21:41:23 +0900

    update create

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const path = require('path')
+const crypto = require('crypto')
 const PORT = process.env.PORT || 5000
 
 express()
@@ -7,4 +8,15 @@ express()
   .set('views', path.join(__dirname, 'views'))
   .set('view engine', 'ejs')
   .get('/', (req, res) => res.render('pages/index'))
-  .listen(PORT, () => console.log(`Listening on ${ PORT }`))
+  .post("/hook/", (req, res) => {
+    if (crypto.createHmac('SHA256', channelSecret)
+    .update(body).digest('base64');) {
+      return res.status(404);
+    }
+    res.header({
+      Authorization: "3340c51015e5cbeb9700b2a953979ad4"
+    }).json({
+      replyToken: req.body.events.replyToken,
+
+    })}
+  ).listen(PORT, () => console.log(`Listening on ${ PORT }`))
